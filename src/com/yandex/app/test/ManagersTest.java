88dcbab158c6407e168fb0970bf659da456449f8
AlexKortosha <aaalexskortoshka@gmail.com>
2025-08-31 18:29:57 +0600

    Изменить импорт

--- src/com/yandex/app/test/ManagersTest.java
+++ src/com/yandex/app/test/ManagersTest.java
@@ -1,7 +1,7 @@
 package com.yandex.app.test;
 
 import com.yandex.app.model.*;
-import com.yandex.app.serviсe.*;
+import com.yandex.app.service.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
