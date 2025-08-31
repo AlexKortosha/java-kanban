05032f8bfcca2945cb8fef9f00a1174431bb0b29
AlexKortosha <aaalexskortoshka@gmail.com>
2025-08-31 18:14:24 +0600

    исправить codestyle

--- src/com/yandex/app/service/InMemoryHistoryManager.java
+++ src/com/yandex/app/service/InMemoryHistoryManager.java
@@ -27,7 +27,7 @@ public class InMemoryHistoryManager implements HistoryManager {
     public List<Task> getHistory() {
         List<Task> result = new ArrayList<>();
         Node current = head;
-        while(current != null) {
+        while (current != null) {
             result.add(current.task);
             current = current.next;
         }
