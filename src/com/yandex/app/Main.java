250d7135e256b4ef7a8d5be6d130fb23dbfadc96
AlexKortosha <aaalexskortoshka@gmail.com>
2025-08-31 18:28:00 +0600

    Изменить импорт

--- src/com/yandex/app/Main.java
+++ src/com/yandex/app/Main.java
@@ -1,8 +1,4 @@
 package com.yandex.app;
-import com.yandex.app.model.Epic;
-import com.yandex.app.model.Task;
-import com.yandex.app.model.SubTask;
-import com.yandex.app.model.TaskStatus;
 
 import com.yandex.app.serviсe.InMemoryTaskManager;
 import com.yandex.app.serviсe.TaskManager;
