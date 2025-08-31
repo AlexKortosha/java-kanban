44a10b2f2d18243ae1a49751b9def37857806ba8
AlexKortosha <aaalexskortoshka@gmail.com>
2025-08-31 18:20:46 +0600

    Изменить импорт библиотек

--- src/com/yandex/app/service/InMemoryHistoryManager.java
+++ src/com/yandex/app/service/InMemoryHistoryManager.java
@@ -2,9 +2,8 @@ package com.yandex.app.service;
 
 import com.yandex.app.model.Task;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+
 
 
 public class InMemoryHistoryManager implements HistoryManager {
