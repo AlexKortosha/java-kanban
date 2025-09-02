e1b28376dfe33cf5f37143d866799cd065887c46
AlexKortosha <aaalexskortoshka@gmail.com>
2025-09-02 18:49:07 +0600

    Добавить тест

--- test/test/LinckedListOrerationTest.java
+++ test/test/LinckedListOrerationTest.java
@@ -83,5 +83,19 @@ public class LinckedListOrerationTest {
         assertEquals(task1, history.get(1));
     }
 
+    @Test
+    void testAddThreeTasksAndRemoveMiddle() {
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+
+        historyManager.remove(task2.getId());
+        List<Task> history = historyManager.getHistory();
+
+        assertEquals(2, history.size());
+        assertEquals(task1, history.get(0));
+        assertEquals(task3, history.get(1));
+    }
+
 
 }
