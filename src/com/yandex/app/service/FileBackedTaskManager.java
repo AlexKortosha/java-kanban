2c99dd87259adc74426a444c882d7c35affa1507
AlexKortosha <aaalexskortoshka@gmail.com>
2025-09-04 18:19:25 +0600

    Создать метод save

--- src/com/yandex/app/service/FileBackedTaskManager.java
+++ src/com/yandex/app/service/FileBackedTaskManager.java
@@ -1,5 +1,17 @@
 package com.yandex.app.service;
 
-public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager {
+import com.yandex.app.model.Task;
+import com.yandex.app.model.Epic;
+import com.yandex.app.model.SubTask;
+import com.yandex.app.model.TaskType;
+import com.yandex.app.model.TaskStatus;
 
+
+import java.io.*;
+import java.util.List;
+
+public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager {
+    public void save() {
+
+    }
 }
