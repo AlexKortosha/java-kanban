8098822730a60ba51376ac9a3ed723f08d5c80f7
AlexKortosha <aaalexskortoshka@gmail.com>
2025-09-06 17:13:13 +0600

    изменить модификатор доступа

--- src/com/yandex/app/service/InMemoryTaskManager.java
+++ src/com/yandex/app/service/InMemoryTaskManager.java
@@ -8,9 +8,9 @@ import java.util.*;
 
 
 public class InMemoryTaskManager implements TaskManager {
-    private final Map<Integer, Task> tasks = new HashMap<>();
-    private final Map<Integer, SubTask> subTasks = new HashMap<>();
-    private final Map<Integer, Epic> epics = new HashMap<>();
+    protected final Map<Integer, Task> tasks = new HashMap<>();
+    protected final Map<Integer, SubTask> subTasks = new HashMap<>();
+    protected final Map<Integer, Epic> epics = new HashMap<>();
 
     private final HistoryManager historyManager;
 
